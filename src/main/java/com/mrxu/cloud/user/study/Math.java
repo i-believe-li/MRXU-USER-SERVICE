eb32d7131c1fad9fa87b7d368dbcb1962076b5fa
徐智伟 <xuzhiwei@i-focusing.com>
2018-05-16 18:13:38 +0800

    * #0000: 配置修改

---
+++ src/main/java/com/mrxu/cloud/user/study/Math.java
@@ -0,0 +1,13 @@
+package com.mrxu.cloud.user.study;
+
+/**
+ * @author ifocusing-xuzhiwei
+ * @since 2018/5/15
+ */
+public class Math {
+    public static void main(String[] args) {
+        long i = 121111111L;
+        Long j = 121111111L;
+        System.out.println(i == j);
+    }
+}
